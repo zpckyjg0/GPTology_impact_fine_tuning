0145a72a83f43251a9eb207b4d99b189b250d9e7
zpckyjg0 <91918231+zpckyjg0@users.noreply.github.com>
2022-06-23 10:59:51 -0400

    add cka analysis

--- analysis.py
+++ analysis.py
@@ -1,16 +1,3 @@
-#############################################################
-#############################################################
-#############################################################    
-"""CODE FOR THIS FILE WAS ADAPTED FROM: 
- https://github.com/INK-USC/IsoBN
- The original code was meant for fine tuning
- BERT on GLUE tasks and looks at whether adding
- an isotropic regularizer improves performance""" 
-#############################################################
-#############################################################
-#############################################################
-
-
 #PyTorch & Numpy
 import torch 
 from torch.utils.data import DataLoader 
@@ -45,127 +32,6 @@ def collate_fn(batch):
     labels = torch.tensor(labels, dtype=torch.long)
     outputs = { "input_ids": input_ids, "attention_mask": input_mask, "labels": labels }
     return outputs
-    
-
-#TODO Finish editing the testing flag: for now store the incorrect/correct examples
-#want a way extract sentences from the batch where labels are incorrect. 
-#Per element boolean if the preds and labels match 
-
-#TODO Do we want to add a wandb wrapper??? ]
-#TODO at a minimum, we need to store/print train&val loss 
-
-def eval(args, data, model, tokenizer, test=False):
-    # Set model to eval mode. Load metric and create data loader.  
-    print("Evaluating") 
-    model.eval() 
-    eval_loader = DataLoader(data, batch_size=args.batch_size, collate_fn=collate_fn)
-    
-    # Send model to gpu if available
-    device = 'cuda:0' if torch.cuda.is_available() else 'cpu'
-    model.to(device)
-    
-    # Lists to store results. 
-    preds_list = []
-    labels_list = []
-    
-    # collect correct and incorrect examples  
-    correct = {} 
-    correct["sentence"] = []
-    correct["label"] = []
-    correct["prob"] = []  
-    
-    incorrect = {} 
-    incorrect["sentence"] = []
-    incorrect["label"] = []
-    incorrect["prob"] = [] 
-    
-    # main EVAL loop 
-    for idx, batch in enumerate(eval_loader):
-        # send batch to device  
-        batch = {key: value.to(device) for key, value in batch.items()} 
-       
-        # Set model to eval and run input batches with no_grad to disable gradient calculations   
-        model.eval()
-        with torch.no_grad():
-            outputs = model(**batch) 
-            logits = outputs.logits   
-       # Store Predictions and Labels
-        preds = logits.argmax(axis=1)        
-        preds = preds.detach().cpu().numpy()  
-        preds_list.append(preds)   
-        labels = batch["labels"].detach().cpu().numpy() 
-        labels_list.append(labels)  
-        probs = torch.nn.functional.softmax(logits, dim=1)  
-         
-        # Get indices of correct and incorrect labels 
-        correct_indx = np.where(preds == labels)[0] 
-        incorrect_indx = list(np.where(preds != labels)[0]) 
-        ids = batch['input_ids'] 
-         
-        for i in correct_indx:
-            ex = ids[i]  
-            ex = ex[ex != 0] 
-            correct["sentence"].append(tokenizer.decode(ex))
-            correct["label"].append(labels[i])
-            correct["prob"].append(max(probs[i]))
-        
-        for i in incorrect_indx:
-            ex = ids[i]  
-            ex = ex[ex != 0] 
-            incorrect["sentence"].append(tokenizer.decode(ex))
-            incorrect["label"].append(labels[i])
-            incorrect["prob"].append(max(probs[i])) 
-    # Saving results          
-    with open(args.task + "_correct.pickle", "wb") as handle:  
-        pickle.dump(correct, handle)   
-    with open(args.task + "_incorrect.pickle", "wb") as handle:   
-        pickle.dump(incorrect, handle) 
-    
-    # Compute Accuracy 
-    preds = np.concatenate(preds_list, axis=0)
-    labels = np.concatenate(labels_list, axis=0)
-    acc = (preds == labels).sum()/len(preds)
-
-    return acc 
-
-def train(args, train_data, eval_data, model):
-    # Instantiate Data Loader, optimizer & scheduler 
-    train_loader = DataLoader(train_data, batch_size=args.batch_size, collate_fn=collate_fn, shuffle=True, drop_last=True) 
-    optimizer = args.optimizer(model.parameters(), lr=1e-5) 
-    #scheduler = args.scheduler
-    
-    # Send model to gpu if available  
-    device = 'cuda:0' if torch.cuda.is_available() else 'cpu'
-    model.to(device)
-
-    # Main TRAINING loop
-    for epoch in range(args.num_epochs):
-        for idx, batch in enumerate(tqdm(train_loader)):
-            # Set model into training mode  
-            model.train() 
-            
-            # Send batch to gpu if available 
-            batch = {key: value.to(device) for key, value in batch.items()} 
-             
-            # Input batch, calculate loss and backprop 
-            outputs = model(**batch)
-            loss = outputs.loss
-            loss.backward() 
-            #scheduler.step() 
-            optimizer.step()
-            
-            # Clears all gradient calculations for the next round of training 
-            model.zero_grad()   
-        
-        # After each epoch, evaluate on validation data 
-        #results = eval(args, data=eval_data, model=model)         
-         
-        print("{} out of {} epochs completed".format(epoch+1, args.num_epochs))
-        print("Loss: {}".format(loss.item())) 
-        #print("Performance on Validation Set: {}".format(results)) 
-        print("------------------------------------------------------------------") 
-    # TODO: save model 
-
 
 def get_hidden_states_qnli_gpt2(tokenizer, model, qnli_data): 
     # Create dictionary
@@ -181,28 +47,6 @@ def get_hidden_states_qnli_gpt2(tokenizer, model, qnli_data):
             hidden_states[12].append(np.squeeze(states[12])[-1])       
     return hidden_states
 
-"""
-def get_hidden_states_qnli_gpt2(tokenizer, model, qnli_data): 
-    # Create dictionary
-    model.eval()  
-    hidden_states = {} 
-    hidden_states[12] = []  
-    
-    # DataLoader & Device 
-    data_loader = DataLoader(qnli_data, batch_size=8, collate_fn=collate_fn, shuffle=False)    
-    device = 'cuda:0' if torch.cuda.is_available() else 'cpu'
-    model.to(device)
-     
-    for idx, batch in enumerate(data_loader):
-        batch = {key: value.to(device) for key, value in batch.items()} 
-        with torch.no_grad():
-            outputs = model(**batch, output_hidden_states=True)
-            states = outputs.hidden_states 
-            hidden_states[12].append(states[12][:,-1,:])       
-    
-    return hidden_states
-"""
-
 def get_hidden_states_sst2_gpt2(tokenizer, model, sst2_data):
     # Create dictionary
     model.eval() 
@@ -216,37 +60,6 @@ def get_hidden_states_sst2_gpt2(tokenizer, model, sst2_data):
             hidden_states[12].append(np.squeeze(states[12])[-1])         
     return hidden_states
 
-def get_hidden_states_sst2_bert(tokenizer, model, sst2_data): 
-    # Create dictionary
-    model.eval() 
-    hidden_states = {}
-    for i in range(1,13):
-        hidden_states[i] = []  
-    for sentence in sst2_data['validation']['sentence']: 
-        tokens_tensor = tokenizer(sentence, return_tensors='pt', padding=False, max_length=256, truncation=True)
-        with torch.no_grad():
-            outputs = model(**tokens_tensor, output_hidden_states=True)
-            states = outputs.hidden_states
-            for i in range(1, len(states)):
-                hidden_states[i].append(np.squeeze(states[i])[0])         
-    return hidden_states
-
-def get_hidden_states_qnli_bert(tokenizer, model, qnli_data):
-    """Calculates the probability negative"""
-    # Create dictionary
-    model.eval() 
-    hidden_states = {}
-    
-    hidden_states[12] = []  
-    for j in range(len(qnli_data['validation']['label'])):
-        sentence = (qnli_data['validation']['question'][j], qnli_data['validation']['sentence'][j])
-        tokens_tensor = tokenizer(*sentence, return_tensors='pt', padding=False, max_length=256, truncation=True)
-        with torch.no_grad():
-            outputs = model(**tokens_tensor, output_hidden_states=True)
-            states = outputs.hidden_states 
-            hidden_states[12].append(np.squeeze(states[12])[0])       
-    return hidden_states
-
 def get_pred_sst2(sentence, tokenizer, model):
    
     tokens_tensor = tokenizer(*sentence, return_tensors='pt', padding=False, max_length=256, truncation=True)
@@ -258,8 +71,7 @@ def get_pred_sst2(sentence, tokenizer, model):
         logits = outputs.logits
         probs = torch.nn.functional.softmax(logits, dim=1)
     return probs.argmax().item()
-
-
+   
 def get_pred_qnli(sentence, tokenizer, model):
 
     tokens_tensor = tokenizer(*sentence, return_tensors='pt', padding=False, max_length=256, truncation=True) 
@@ -270,6 +82,155 @@ def get_pred_qnli(sentence, tokenizer, model):
         logits = outputs.logits
         probs = torch.nn.functional.softmax(logits, dim=1)
     return probs.argmax().item()
+   
+#############################################################
+#############################################################
+#############################################################    
+"""CODE FOR THE CKA ANALYSIS WAS TAKEN FROM THE AUTHOR'S 
+GOOGLE COLAB DEMO: 
+https://colab.research.google.com/github/google-research/google-research/blob/master/representation_similarity/Demo.ipynb#scrollTo=zeI3il6JRM-L
+""" 
+#############################################################
+#############################################################
+#############################################################
+
+def gram_linear(x):
+    """Compute Gram (kernel) matrix for a linear kernel.
+    Args:
+    x: A num_examples x num_features matrix of features.
+    Returns:
+    A num_examples x num_examples Gram matrix of examples.
+    """
+    return x.dot(x.T)
+
+
+def gram_rbf(x, threshold=1.0):
+    """Compute Gram (kernel) matrix for an RBF kernel.
+    Args:
+    x: A num_examples x num_features matrix of features.
+    threshold: Fraction of median Euclidean distance to use as RBF kernel
+      bandwidth. (This is the heuristic we use in the paper. There are other
+      possible ways to set the bandwidth; we didn't try them.)
+      Returns:
+      A num_examples x num_examples Gram matrix of examples.
+      """
+    dot_products = x.dot(x.T)
+    sq_norms = np.diag(dot_products)
+    sq_distances = -2 * dot_products + sq_norms[:, None] + sq_norms[None, :]
+    sq_median_distance = np.median(sq_distances)
+    return np.exp(-sq_distances / (2 * threshold ** 2 * sq_median_distance))
+
+
+def center_gram(gram, unbiased=False):
+    """Center a symmetric Gram matrix.
+    This is equvialent to centering the (possibly infinite-dimensional) features
+    induced by the kernel before computing the Gram matrix.
+    Args:
+    gram: A num_examples x num_examples symmetric matrix.
+    unbiased: Whether to adjust the Gram matrix in order to compute an unbiased
+    estimate of HSIC. Note that this estimator may be negative.
+    Returns:
+    A symmetric matrix with centered columns and rows.
+    """
+    if not np.allclose(gram, gram.T):
+        raise ValueError('Input must be a symmetric matrix.')
+    gram = gram.copy()
+        
+    if unbiased:
+        # This formulation of the U-statistic, from Szekely, G. J., & Rizzo, M.
+        # L. (2014). Partial distance correlation with methods for dissimilarities.
+        # The Annals of Statistics, 42(6), 2382-2412, seems to be more numerically
+        # stable than the alternative from Song et al. (2007).
+        n = gram.shape[0]
+        np.fill_diagonal(gram, 0)
+        means = np.sum(gram, 0, dtype=np.float64) / (n - 2)
+        means -= np.sum(means) / (2 * (n - 1))
+        gram -= means[:, None]
+        gram -= means[None, :]
+        np.fill_diagonal(gram, 0)
+    else:
+        means = np.mean(gram, 0, dtype=np.float64)
+        means -= np.mean(means) / 2
+        gram -= means[:, None]
+        gram -= means[None, :]
+
+    return gram
+
+
+def cka(gram_x, gram_y, debiased=False):
+    """Compute CKA.
+
+      Args:
+        gram_x: A num_examples x num_examples Gram matrix.
+        gram_y: A num_examples x num_examples Gram matrix.
+        debiased: Use unbiased estimator of HSIC. CKA may still be biased.
+
+      Returns:
+      The value of CKA between X and Y.
+      """
+    gram_x = center_gram(gram_x, unbiased=debiased)
+    gram_y = center_gram(gram_y, unbiased=debiased)
+
+    # Note: To obtain HSIC, this should be divided by (n-1)**2 (biased variant) or
+    # n*(n-3) (unbiased variant), but this cancels for CKA.
+    scaled_hsic = gram_x.ravel().dot(gram_y.ravel())
+
+    normalization_x = np.linalg.norm(gram_x)
+    normalization_y = np.linalg.norm(gram_y)
+    return scaled_hsic / (normalization_x * normalization_y)
+
+
+def _debiased_dot_product_similarity_helper(
+    xty, sum_squared_rows_x, sum_squared_rows_y, squared_norm_x, squared_norm_y,
+    n):
+    """Helper for computing debiased dot product similarity (i.e. linear HSIC)."""
+    # This formula can be derived by manipulating the unbiased estimator from
+    # Song et al. (2007).
+    return (
+      xty - n / (n - 2.) * sum_squared_rows_x.dot(sum_squared_rows_y)
+      + squared_norm_x * squared_norm_y / ((n - 1) * (n - 2)))
+
+def feature_space_linear_cka(features_x, features_y, debiased=False):
+    """Compute CKA with a linear kernel, in feature space.
+    This is typically faster than computing the Gram matrix when there are fewer
+    features than examples.
+
+  Args:
+    features_x: A num_examples x num_features matrix of features.
+    features_y: A num_examples x num_features matrix of features.
+    debiased: Use unbiased estimator of dot product similarity. CKA may still be
+      biased. Note that this estimator may be negative.
+
+    Returns:
+    The value of CKA between X and Y.
+  """
+    features_x = features_x - np.mean(features_x, 0, keepdims=True)
+    features_y = features_y - np.mean(features_y, 0, keepdims=True)
+
+    dot_product_similarity = np.linalg.norm(features_x.T.dot(features_y)) ** 2
+    normalization_x = np.linalg.norm(features_x.T.dot(features_x))
+    normalization_y = np.linalg.norm(features_y.T.dot(features_y))
+
+    if debiased:
+        n = features_x.shape[0]
+        # Equivalent to np.sum(features_x ** 2, 1) but avoids an intermediate array.
+        sum_squared_rows_x = np.einsum('ij,ij->i', features_x, features_x)
+        sum_squared_rows_y = np.einsum('ij,ij->i', features_y, features_y)
+        squared_norm_x = np.sum(sum_squared_rows_x)
+        squared_norm_y = np.sum(sum_squared_rows_y)
+
+        dot_product_similarity = _debiased_dot_product_similarity_helper(
+            dot_product_similarity, sum_squared_rows_x, sum_squared_rows_y,
+            squared_norm_x, squared_norm_y, n)
+        normalization_x = np.sqrt(_debiased_dot_product_similarity_helper(
+            normalization_x ** 2, sum_squared_rows_x, sum_squared_rows_x,
+            squared_norm_x, squared_norm_x, n))
+        normalization_y = np.sqrt(_debiased_dot_product_similarity_helper(
+            normalization_y ** 2, sum_squared_rows_y, sum_squared_rows_y,
+            squared_norm_y, squared_norm_y, n))
+
+    return dot_product_similarity / (normalization_x * normalization_y)
+
 
 def main():
     parser = argparse.ArgumentParser()
@@ -298,12 +259,21 @@ def main():
     tokenizer.pad_token = tokenizer.eos_token      
     #Specifying the pad token  
     model.config.pad_token_id = model.config.eos_token_id
-
-    #TODO: LOAD ALL HIDDEN STATES FOR CKA 
-    #TODO ADD CKA FLAG & FUNCTIONS. 
-    
     device = 'cuda:0' if torch.cuda.is_available() else 'cpu' 
     
+
+    
+    if args.cka==True:
+        # Load Hidden states for the validation data.
+        val = hickle.load(args.model + "_"+ args.task  + "_validation_hidden_states.hickle") 
+        scores = np.zeros((12,12))
+        # Note that the first layer here is the LEXICAL embedding which is why we skip it. 
+        for i in range(1,13):
+            for j in range(1,13):
+                scores[i-1][j-1] = cka(gram_linear(np.vstack(val[i])), gram_linear(np.vstack(val[j]))
+        hickle.dump(scores, args.task + "_cka.hickle", mode='w')
+        return None
+ 
     if args.rogue_dim==True:    
         # load dataset labels 
         if args.task=="sst2":
